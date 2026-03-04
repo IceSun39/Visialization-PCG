6478a39ca7dc7167a51dda4295b195d46b482880
Vlad Kolesnik <kolesnikvlad416@gmail.com>
2026-03-04 18:52:18 +0200

    created mainScene.fxml, added controller for main scene

--- src/main/java/com/vlad/Controllers/mainSceneController.java
+++ src/main/java/com/vlad/Controllers/mainSceneController.java
@@ -1,20 +1,88 @@
 package com.vlad.Controllers;
 
-import javafx.event.ActionEvent;
+import com.vlad.Model.PCG32;
 import javafx.fxml.FXML;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
-import javafx.scene.control.TextArea;
+import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class mainSceneController {
+
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Button stopButton;
+    @FXML
+    private Slider speedSlider;
+    @FXML
+    private TextArea countDots;
+    @FXML
+    private Label countPaintedDots;
+    @FXML
+    private Label errorUnderZeroLabel;
+    @FXML
+    private Label errorNotNumberLabel;
+
+    private long samples;
+
+    private PCG32 rng;
+
+    private GraphicsContext gc;
+
+    @FXML
+    void initialize(){
+        gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        gc.setFill(Color.WHITE);
+
+        rng = new PCG32(42L, 54L);
+    }
+
+    @FXML
+    public void startDrawing(ActionEvent event) throws IOException {
+        String input = countDots.getText();
+        try {
+            samples = Long.parseLong(input);
+
+            if(samples < 0){
+                throw new IllegalArgumentException();
+            }
+
+            for (int i = 0; i < samples; i++) {
+                double x = rng.nextDouble();
+                double y = rng.nextDouble();
+
+                double px = x * canvas.getWidth();
+                double py = y * canvas.getHeight();
+
+
+                gc.fillRect(px, py, 1, 1);
+            }
+        }catch (NumberFormatException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Помилка введення");
+            alert.setHeaderText(null);
+            alert.setContentText("Кількість точок повинна бути числом!");
+            alert.showAndWait();
+        }catch (IllegalArgumentException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Помилка введення");
+            alert.setHeaderText(null);
+            alert.setContentText("Кількість точок повинна бути додатним числом!");
+            alert.showAndWait();
+        }
+
+    }
 
 }
---
+++ src/main/java/com/vlad/Main.java
@@ -0,0 +1,72 @@
+package com.vlad;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+//public class PCGVisualizer extends Application {
+//
+//    private static final int WIDTH = 800;
+//    private static final int HEIGHT = 800;
+//    private static final int SAMPLES = 300_000;
+//
+//    @Override
+//    public void start(Stage stage) {
+//
+//        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+//        GraphicsContext gc = canvas.getGraphicsContext2D();
+//
+//        gc.setFill(Color.BLACK);
+//        gc.fillRect(0, 0, WIDTH, HEIGHT);
+//
+//        gc.setFill(Color.WHITE);
+//
+//        PCG32 rng = new PCG32(42L, 54L);
+//
+//        for (int i = 0; i < SAMPLES; i++) {
+//            double x = rng.nextDouble();
+//            double y = rng.nextDouble();
+//
+//            double px = x * WIDTH;
+//            double py = y * HEIGHT;
+//
+//            gc.fillRect(px, py, 1, 1);
+//        }
+//
+//        StackPane root = new StackPane(canvas);
+//        Scene scene = new Scene(root);
+//
+//        stage.setTitle("PCG Scatter Plot");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch();
+//    }
+//}
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+            Parent root = FXMLLoader.load(getClass().getResource("/com/vlad/View/mainScene.fxml"));
+            stage.setTitle("Генератор PCG");
+            stage.setScene(new Scene(root, 900, 500));
+
+            stage.setOnCloseRequest(event -> {
+                event.consume();
+            });
+
+            stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
